fbbe8164381c2db1b814c134f200b5e10cfb8ff7
Craig Miller <craigmiller160@gmail.com>
2021-05-14 15:31:22 -0400

    Added dev server proxy

--- project.config.js
+++ project.config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'Stock Tracker',
     devServerPort: 3000,
     devServerHttps: true,
-    publicPath: '/stock-tracker'
+    publicPath: '/stock-tracker',
+    devServerProxy: {
+        '/stock-tracker/api': {
+            target: 'https://localhost:8080',
+            changeOrigin: true,
+            secure: false,
+            pathRewrite: {
+                '^/stock-tracker/api': ''
+            },
+            logLevel: 'debug'
+        }
+    }
 };
