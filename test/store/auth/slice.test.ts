a23cf4f2a00a2005fc117ece9be2990f5f7d2123
Craig Miller <craigmiller160@gmail.com>
2021-05-14 15:23:21 -0400

    Implemented a test

--- test/store/auth/slice.test.ts
+++ test/store/auth/slice.test.ts
@@ -1,5 +1,24 @@
+import authSlice, { initialState as authInitState } from '../../../src/store/auth/slice';
+import { AuthUser } from '../../../src/types/auth';
+import * as O from 'fp-ts/es6/Option';
+
 describe('auth slice', () => {
-	it('test', () => {
-		throw new Error();
+	it('setUserData', () => {
+		const authUser: AuthUser = {
+			username: 'user',
+			firstName: 'firstName',
+			lastName: 'lastName',
+			roles: []
+		};
+
+		const result = authSlice.reducer(
+			authInitState,
+			authSlice.actions.setUserData(O.some(authUser))
+		);
+		expect(result).toEqual({
+			...authInitState,
+			userData: O.some(authUser),
+			hasChecked: true
+		});
 	});
 });
