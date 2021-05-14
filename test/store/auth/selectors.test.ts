b7198d2e990b480695ff6691f26a8accf2b86b02
Craig Miller <craigmiller160@gmail.com>
2021-05-14 15:26:09 -0400

    Stubbed out selector tests

---
+++ test/store/auth/selectors.test.ts
@@ -0,0 +1,12 @@
+
+describe('auth selectors', () => {
+    describe('isAuthorized', () => {
+        it('true', () => {
+            throw new Error();
+        });
+
+        it('false', () => {
+            throw new Error();
+        });
+    });
+});
