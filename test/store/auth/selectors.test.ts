25c8b9f88ae404367fafa57d248b777a7cde41cb
Craig Miller <craigmiller160@gmail.com>
2021-05-14 15:29:55 -0400

    Implemented more tests

--- test/store/auth/selectors.test.ts
+++ test/store/auth/selectors.test.ts
@@ -1,12 +1,38 @@
+import authSlice from '../../../src/store/auth/slice';
+import { AuthUser } from '../../../src/types/auth';
+import * as O from 'fp-ts/es6/Option';
+import { isAuthorized } from '../../../src/store/auth/selectors';
+import { combineReducers, configureStore, EnhancedStore } from '@reduxjs/toolkit';
+
+const authUser: AuthUser = {
+    username: 'user',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    roles: []
+};
+
+const rootReducer = combineReducers({
+    auth: authSlice.reducer
+});
 
 describe('auth selectors', () => {
+    let store: EnhancedStore;
+    beforeEach(() => {
+        store = configureStore({
+            reducer: rootReducer
+        });
+    });
+
     describe('isAuthorized', () => {
         it('true', () => {
-            throw new Error();
+            store.dispatch(authSlice.actions.setUserData(O.some(authUser)));
+            const result = isAuthorized(store.getState());
+            expect(result).toEqual(true);
         });
 
         it('false', () => {
-            throw new Error();
+            const result = isAuthorized(store.getState());
+            expect(result).toEqual(false);
         });
     });
 });
