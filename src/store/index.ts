d4b6d8425c276338ec465e927b1529b52862d9e9
Craig Miller <craigmiller160@gmail.com>
2021-05-14 15:24:51 -0400

    Wrote selectors

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import { reduxAlertReducer } from '@craigmiller160/react-material-ui-common';
+import authSlice from './auth/slice';
+import { configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+    alert: reduxAlertReducer,
+    auth: authSlice.reducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default configureStore({
+    reducer: rootReducer,
+    devTools: process.env.NODE_ENV !== 'production'
+});
