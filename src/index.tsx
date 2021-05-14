3514c5a30f28e03ca6c17e9d3f90a4157df2a720
Craig Miller <craigmiller160@gmail.com>
2021-05-14 14:29:12 -0400

    Setting up core logic

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import { StrictMode } from 'react';
+import './index.scss';
+
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+    , document.getElementById('root'));
