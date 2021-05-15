6d0cde1a1fff5f3548c22716f2b8e98d72a7f0df
Craig <craigmiller160@gmail.com>
2021-05-15 15:41:35 -0400

    More spinner style improvements

--- src/components/ui/Spinner/index.tsx
+++ src/components/ui/Spinner/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { CircularProgress } from '@material-ui/core';
 import './Spinner.scss';
 
+const style = {
+	width: '100%',
+	height: '100%'
+};
+
 const Spinner = () => (
-    <div className="SpinnerRoot">
-        <CircularProgress />
-    </div>
+	<div className="SpinnerRoot">
+		<CircularProgress style={style} />
+	</div>
 );
 
 export default Spinner;
