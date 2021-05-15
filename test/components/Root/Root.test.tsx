a7f465285e7e613428adae0e5bd6ec9ce7ca4467
Craig <craigmiller160@gmail.com>
2021-05-15 08:46:56 -0400

    Wrote test for Root component

--- test/components/Root/Root.test.tsx
+++ test/components/Root/Root.test.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Root from '../../../src/components/Root';
 import { createStore } from '../../../src/store';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { mockGetAuthUser } from '../../testutils/ajaxmocks/AuthServiceMocks';
+import MockAdapter from 'axios-mock-adapter';
+import ajaxApi from '../../../src/services/AjaxApi';
+
+const mockAjaxApi = new MockAdapter(ajaxApi.instance);
 
 const doRender = () => {
 	const store = createStore();
-	return render(
+	const history = createMemoryHistory();
+	return waitFor(() => render(
 		<Provider store={store}>
-			<Root />
+			<Router history={history }>
+				<Root />
+			</Router>
 		</Provider>
-	);
+	));
 };
 
 describe('Root component', () => {
-	it('test', () => {
-		throw new Error();
+	it('renders after auth check', async () => {
+		mockGetAuthUser(mockAjaxApi);
+		await doRender();
+		expect(screen.queryByText('Content')).toBeInTheDocument();
 	});
 });
