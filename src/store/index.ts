cd881159923f8cd118c0022faa75c27569e00ed7
Craig <craigmiller160@gmail.com>
2021-05-15 12:00:03 -0400

    Added optional pre-loaded state

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,16 +4,17 @@ import authSlice from './auth/slice';
 import { configureStore, EnhancedStore } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
-	alert: reduxAlertReducer, // TODO need to add alert component
+	alert: reduxAlertReducer,
 	auth: authSlice.reducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-export const createStore = (): EnhancedStore<RootState> =>
+export const createStore = (preloadedState: Partial<RootState> = {}): EnhancedStore<RootState> =>
 	configureStore({
 		reducer: rootReducer,
-		devTools: process.env.NODE_ENV !== 'production'
+		devTools: process.env.NODE_ENV !== 'production',
+		preloadedState
 	});
 
 export default createStore();
