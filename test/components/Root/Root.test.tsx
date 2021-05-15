9a2fdfabcffa3525654b16b06bb34b3b84abfd52
Craig <craigmiller160@gmail.com>
2021-05-15 08:23:59 -0400

    Began building test logic

--- test/components/Root/Root.test.tsx
+++ test/components/Root/Root.test.tsx
@@ -1,3 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Root from '../../../src/components/Root';
+import { createStore } from '../../../src/store';
+
+const doRender = () => {
+	const store = createStore();
+	return render(
+		<Provider store={store}>
+			<Root />
+		</Provider>
+	);
+};
+
 describe('Root component', () => {
 	it('test', () => {
 		throw new Error();
