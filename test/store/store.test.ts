9a59567a131718c36396af1d10ad6fc5ea57e15c
Craig <craigmiller160@gmail.com>
2021-05-15 12:11:05 -0400

    Wrote more tests

--- test/store/store.test.ts
+++ test/store/store.test.ts
@@ -1,10 +1,34 @@
+import { createStore } from '../../src/store';
+import * as O from 'fp-ts/es6/Option';
 
 describe('redux store', () => {
     it('has correct initial state without preloaded state', () => {
-        throw new Error();
+        const store = createStore();
+        expect(store.getState()).toEqual(expect.objectContaining({
+            auth: {
+                userData: O.none,
+                hasChecked: false
+            }
+        }));
     });
 
     it('has correct initial state with preloaded state', () => {
-        throw new Error();
+        const store = createStore({
+            auth: {
+                userData: O.none,
+                hasChecked: true
+            }
+        });
+        expect(store.getState()).toEqual(expect.objectContaining({
+            auth: {
+                userData: O.none,
+                hasChecked: true
+            },
+            alert: {
+                show: false,
+                type: 'success',
+                message: ''
+            }
+        }));
     });
 });
